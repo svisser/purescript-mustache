57190b2ab98690be4ea26a71b1fc99ef54bc0ac2
Simeon Visser <simeon87@gmail.com>
2015-08-06 23:36:30 +0100

    Added documentation to available functions in package

--- src/Mustache.purs
+++ src/Mustache.purs
@@ -21,18 +21,28 @@ foreign import showTokenElementImpl :: TokenElement -> String
 instance showTokenElement :: Show TokenElement where
   show = showTokenElementImpl
 
+-- | Effect that represents side effects within mustache.js.
+-- |
+-- | This is used to indicate caching that takes place within mustache.js.
 foreign import data MustacheEffect :: !
 
+-- | Name of the mustache.js dependency.
 foreign import name :: String
 
+-- | Version string as specified in mustache.js.
 foreign import version :: String
 
+-- | Default mustache.js tags (default: array with "{{" and "}}").
 foreign import tags :: Array Tag
 
+-- | Precompile a template and store parsed token in mustache.js's cache.
 foreign import parse :: forall e. Template -> Array Tag -> Eff (mustache :: MustacheEffect | e) (Array Token)
 
+-- | Render the template with the given view and partial templates.
 foreign import render :: forall e. Template -> View -> Partials -> Eff (mustache :: MustacheEffect | e) String
 
+-- | Escape various characters as HTML entities in the given string.
 foreign import escape :: String -> String
 
+-- | Clear the internal cache used by mustache.js.
 foreign import clearCache :: forall e. Eff (mustache :: MustacheEffect | e) Unit
