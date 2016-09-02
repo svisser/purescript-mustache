146833bdff50c60a13844e7da9d4fa59bad9beef
Alex Berg <chexxor@gmail.com>
2016-09-02 18:35:32 -0500

    Remove StrMap. Add some tests.

--- test/Mustache.purs
+++ test/Mustache.purs
@@ -1,22 +1,57 @@
 module Test.Main where
 
-import Prelude (bind, Unit)
+import Prelude (bind, Unit, show, ($))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log, CONSOLE)
-import Data.StrMap as S
-import Mustache (render, MustacheEffect)
+import Data.Eq ((==))
+import Data.Ord ((<))
+import Data.Semiring ((+))
+import Test.QuickCheck (assertEquals)
+import Mustache (render, MustacheEffect, Partials(), View())
 
-view :: S.StrMap String
-view = S.insert "name" "John" (S.insert "value" "10000" S.empty)
+templateSimple :: String
+templateSimple = "<div>Hello {{name}}! You have just won ${{value}}!</div>"
 
-template = "Hello {{name}}! You have just won ${{value}}!"
+templatePartial :: String
+templatePartial = "<div>Hello {{name}} from {{>cityPartial}}! You have just won ${{value}}!</div>"
 
-// This is a poor test. Please add better tests.
+templateComment :: String
+templateComment = "<div>Hello {{!name}} (commented)!</div>"
+
+templateRepeat :: String
+templateRepeat = "<list>{{#items}}<name>{{name}}</name><price>${{price}}</price>{{/items}}</list>"
+
+view :: View
+  ( name :: String
+  , value :: String
+  , cityName :: String
+  , items :: Array {name :: String, price :: Int}
+  )
+view =
+  { name: "John"
+  , value: "10000"
+  , cityName: "Citytown"
+  , items: [{name: "item1", price: 1}, {name: "item2", price: 2}]
+  }
+
+partials :: Partials (cityPartial :: String)
+partials =
+  { cityPartial: "<span>{{cityName}}</span>"
+  }
+
+-- This is a poor test. Please add assertions and better tests.
 main :: forall a. Eff
   ( mustache :: MustacheEffect
   , console :: CONSOLE
   | a
   ) Unit
 main = do
-  result <- render template view S.empty
-  log result
+  resultSimple <- render templateSimple view {}
+  --show $ assertEquals resultSimple "<div>Hello John! You have just won $10000!</div>"
+  log resultSimple
+  resultPartial <- render templatePartial view partials
+  log resultPartial
+  resultComment <- render templateComment view partials
+  log resultComment
+  resultRepeat <- render templateRepeat view partials
+  log resultRepeat
